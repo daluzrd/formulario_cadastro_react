93764b371ba869fad08703d20d12de2438618bbf
Lucas Luz <LUCAS_LUZ@jabil.com>
2021-03-31 14:51:35 -0300

    Validação e formatação do CPF

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./App.css";
 import { Container } from "@material-ui/core";
 
-class App extends Component {
-	render() {
-		return (
-			<Container maxWidth="sm">
-				<h1>Formulário de cadastro</h1>
-				<FormularioCadastro />
-			</Container>
-		);
-	}
+function App() {
+	return (
+		<Container maxWidth="sm">
+			<h1>Formulário de cadastro</h1>
+			<FormularioCadastro />
+		</Container>
+	);
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	Button,
 	FormControlLabel,
@@ -7,27 +7,78 @@ import {
 	TextField,
 } from "@material-ui/core";
 
+function formataCpf(cpf) {
+	cpf = cpf.replace(/\D/g, "");
+	if (cpf.length > 3)
+		cpf = cpf.substring(0, 3) + "." + cpf.substring(3, cpf.length); //000.000.000-00
+	if (cpf.length > 7)
+		cpf = cpf.substring(0, 7) + "." + cpf.substring(7, cpf.length);
+	if (cpf.length > 11)
+		cpf = cpf.substring(0, 11) + "-" + cpf.substring(11, cpf.length);
+	if (cpf.length > 14) cpf = cpf.substring(0, 14);
+	return cpf;
+}
+
+function validaDigito10(cpf) {
+	cpf = cpf.replace(/\D/g, "");
+	let total = 0;
+	let resto;
+	if (cpf.length === 11) {
+		for (let i = 0; i < 9; i++) total += Number.parseInt(cpf[i]) * (i + 1);
+		resto = total % 11;
+
+		if (resto === 10 || resto === 11) resto = 0;
+		if (resto === Number.parseInt(cpf[9])) return true;
+	}
+	return false;
+}
+
+function validaDigito11(cpf) {
+	cpf = cpf.replace(/\D/g, "");
+	let total = 0;
+	let resto;
+	if (cpf.length === 11) {
+		for (let i = 0; i < 10; i++) {
+			let multiplicacao = Number.parseInt(cpf[i]) * i;
+			total += multiplicacao;
+		}
+		resto = total % 11;
+
+		if (resto === 10 || resto === 11) resto = 0;
+		if (resto === Number.parseInt(cpf[10])) return true;
+	}
+	return false;
+}
+
+function validaCpf(cpf) {
+	if (!validaDigito10(cpf)) return false;
+	return validaDigito11(cpf);
+}
+
 function FormularioCadastro() {
-	let nome = "";
-	let sobrenome = "";
-	let cpf = "";
-	let promocoes = true;
-	let novidades = true;
+	const [nome, setNome] = useState("");
+	const [sobrenome, setSobrenome] = useState("");
+	const [cpf, setCpf] = useState("");
+	const [promocoes, setPromocoes] = useState(true);
+	const [novidades, setNovidades] = useState(true);
 
 	return (
 		<form
 			onSubmit={(event) => {
 				event.preventDefault();
-				console.log(nome);
-				console.log(sobrenome);
-				console.log(cpf);
-				console.log(promocoes);
-				console.log(novidades);
+				if (validaCpf(cpf)) {
+					console.log(nome);
+					console.log(sobrenome);
+					console.log(cpf);
+					console.log(promocoes);
+					console.log(novidades);
+				} else console.log("CPF inválido!");
 			}}
 		>
 			<TextField
+				value={nome}
 				onChange={(event) => {
-					nome = event.target.value;
+					setNome(event.target.value);
 				}}
 				type="text"
 				id="nome"
@@ -35,10 +86,12 @@ function FormularioCadastro() {
 				variant="outlined"
 				margin="normal"
 				fullWidth
+				required={true}
 			/>
 			<TextField
+				value={sobrenome}
 				onChange={(event) => {
-					sobrenome = event.target.value;
+					setSobrenome(event.target.value);
 				}}
 				type="text"
 				id="sobrenome"
@@ -48,8 +101,11 @@ function FormularioCadastro() {
 				fullWidth
 			/>
 			<TextField
+				value={cpf}
 				onChange={(event) => {
-					cpf = event.target.value;
+					let tempCpf = event.target.value;
+					tempCpf = formataCpf(tempCpf);
+					setCpf(tempCpf);
 				}}
 				type="text"
 				id="cpf"
@@ -57,29 +113,32 @@ function FormularioCadastro() {
 				variant="outlined"
 				margin="normal"
 				fullWidth
+				required={true}
 			/>
 			<FormGroup row>
 				<FormControlLabel
+					value={promocoes}
 					id="promocoes"
 					label="Promoções"
 					control={
 						<Switch
 							color="primary"
 							onChange={(event) => {
-								promocoes = event.target.value;
+								setPromocoes(event.target.checked);
 							}}
 							defaultChecked={true}
 						/>
 					}
 				/>
 				<FormControlLabel
+					value={novidades}
 					id="novidades"
 					label="Novidades"
 					control={
 						<Switch
 							color="primary"
 							onChange={(event) => {
-								novidades = event.target.value;
+								setNovidades(event.target.checked);
 							}}
 							defaultChecked={true}
 						/>
